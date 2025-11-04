34c5319786fa37d998105a21332c2b140773a906
NathanCorcellut <nathan.corcellut@gmail.com>
2025-11-04 16:31:43 +0100

    c2 working well

--- 1104_Correl_Plot_CSV/src/main.py
+++ 1104_Correl_Plot_CSV/src/main.py
@@ -7,10 +7,10 @@ import scipy
 import scipy.stats
 import math
 
-def generateTypedListFromExcelFile(nameOfFilePath, positionOfFirstValueLine, positionOfFirstValueColumn, 
+def generateTypedContentFromExcelFile(nameOfFilePath, positionOfFirstValueLine, positionOfFirstValueColumn, 
                                    previousCellSeparator = ';', previsousDecimalSeparator = ',',
                                    desiredType = float):
-    contenu = []
+    content = []
 
     with open(nameOfFilePath,"r") as file:
         idx_line = 0
@@ -28,19 +28,69 @@ def generateTypedListFromExcelFile(nameOfFilePath, positionOfFirstValueLine, pos
                     info = ""
                 else:
                     info += caracter
-            contenu.append(liste)
+            content.append(liste)
             idx_line += 1
 
-    return contenu
+    return content
+
+
 
 
 def extractXYListsFromTxtFile(nameOfFilePath):
-    liste =[]
-    return liste
+    # It returns a list of list of list as built:
+    # [ [ ["label_1"], [X_1], [Y_1] ] , 
+    #   [ ["label_2"], [X_2], [Y_2] ] ,
+    #   [ ["label_3"], [X_3], [Y_3] ] ]
 
+    final_liste = []
+    with open(nameOfFilePath,"r") as file:
+        label, X, Y = [], [], []
+        writing = False
+        for line in file:
+            if line == ')\n':
+                writing = False
+                final_liste.append([label, X, Y])
+                label, X, Y = [], [], []
+
+            elif writing == True :
+                x, temp = line.split('\t')
+                y = temp.split('\n')[0]
+                X.append(float(x))
+                Y.append(float(y))
+
+            elif "label " in line:
+                writing = True
+                temp = line.split(' ')[1]
+                lab = temp.split(')')[0]
+                label.append(lab)
+                
+    return final_liste
+
+
+def generateGraphsFromXYLists(XYList, nameOfOutputPathAndName):
+    for graph in XYList:
+        label, X, Y = graph
+        #Graphs
+        plt.plot(X, Y)
+        plt.legend(label)
+
+    #Ajout des titres et de la grille
+    plt.title("Velocity Magnitude") #Titre principal
+    plt.xlabel("Position")          #Titre de l'axe horizontal (X)
+    plt.ylabel("Velocity")  #Titre de l'axe vertical (Y)
+    plt.grid(True)          #Ajout de la grille
+
+    #Sauvegarde du résultat 
+    plt.savefig(nameOfOutputPathAndName) #sous le nom "Q5-loi_rang-taille_terres.png"
+    plt.close() #Fermeture 'propre' du graphe
     
-contenu = generateTypedListFromExcelFile("./data/24A010072_20241015_090520_USZ_MESS.csv", 1, 2)
+contenu = generateTypedContentFromExcelFile("./data/24A010072_20241015_090520_USZ_MESS.csv", 1, 2)
 
+liste = extractXYListsFromTxtFile("./data/velocity_captors.txt")
 
-print(contenu)
+generateGraphsFromXYLists(liste, "./output/Velocity_Position.png")
+
+c2 = extractXYListsFromTxtFile("./data/velocity_profile.txt")
+
+generateGraphsFromXYLists(c2, "./output/3plotsofprofile.png")
 
